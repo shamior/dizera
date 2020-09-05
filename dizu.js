91ae4d2b4b64b265720ff56ea7ea02add04dfea0
[shamior] <[shamior_@hotmail.com]>
2020-09-05 16:47:16 -0300

    Estado inicial

---
+++ dizu.js
@@ -0,0 +1,163 @@
+let printa = console.log
+console.log = (e)=> {let hora = new Date(); printa(hora.toLocaleTimeString()+' ==>', e)}
+let nomeDaJanelaFilha = 'nomezaoTop'
+let wd = window.open('https://www.instagram.com', nomeDaJanelaFilha)
+window.addEventListener('message', receiveMessage, false);
+window.confirm = () => true
+let regExSiteInsta = /https:\/\/instagram\.com|https:\/\/www\.instagram\.com/i
+let relogaPerfis = false
+let secondsToIniate = 120
+
+const contagemPerfil = {
+    "perfil": '',
+    "numeroConfirmados": 0,
+    "numeroPulados": 0,
+    "total": 0
+}
+
+const listaDePerfis = []
+document.querySelectorAll("#instagram_id option").forEach((element, index)=> listaDePerfis.push({'perfil': element.text, 'done': false, 'idx': index}))
+listaDePerfis[0].done = true
+
+function receiveMessage(event){
+    let data = event.data
+    let perfilLogado = getAvailableInstagram()
+    if (data == 'confirma'){
+        confirmaTarefa()
+    }else if (data == 'pula'){
+        pulaTarefa()
+    }else if (data == 'erroPerfil'){
+        console.warn('Perfil selecionado no dizu esta diferente do perfil logado no instagram\nPausando tarefas...')
+    }else if (data == 'logou'){
+        console.log('=== Logado no perfil ' + perfilLogado.perfil + ' ===')
+        let index = perfilLogado.idx
+        console.log('=== Esperando ' + secondsToIniate + ' segundos para iniciar tarefas neste perfil ===')
+        window.setTimeout(init, secondsToIniate * 1000, index, true)
+    }else if (data == 'erroAcharPerfil'){
+        console.warn('Nao foi possivel achar o instagram ' + perfilLogado.perfil + ' nas suas contas salvas do instagram')
+        perfilLogado.done = true;
+        let proxPerfil = getAvailableInstagram()
+        if(proxPerfil == null){
+            console.warn('===== ACABOU OS PERFIS =====')
+            deixaPerfisDisponiveis()
+        }else{
+            printa('\n')
+            console.log('=== Logando no perfil ===' + proxPerfil.perfil)
+            let objeto = {
+                'perfil': proxPerfil.perfil,
+                'action': 'clicarEntrar',
+                'link': 'https://www.instagram.com'
+            }
+            wd.postMessage(objeto, 'https://www.instagram.com')
+        }
+
+    }
+}
+
+function getAvailableInstagram(){
+    let tam = listaDePerfis.length
+    for(let index = 1; index < tam; index++){
+        if (!listaDePerfis[index].done) return listaDePerfis[index]
+    }
+    return null
+}
+
+
+function getSelectedInstagram(){
+    let listaInstagram = document.querySelectorAll("#instagram_id option")
+    let tam = listaInstagram.length
+    for (let insta = 0; insta < tam; insta++){
+        if (listaInstagram[insta].selected) return listaInstagram[insta]
+    }
+    return null
+}
+
+function init(index=1){
+    relogaPerfis = true
+    document.querySelectorAll("#instagram_id option")[index].selected = true
+    window.setTimeout(iniciaTarefas, 3000, true)
+}
+
+function iniciaTarefas(r=false){
+    let hora = new Date()
+    console.log('=== Iniciando... ===')
+    relogaPerfis = r
+    contagemPerfil.perfil = getSelectedInstagram().text
+    document.querySelector('#iniciarTarefas').click()
+    window.setTimeout(comunicaComOInsta, 7000)
+}
+
+function comunicaComOInsta(){
+    let comTarefas = document.querySelector('.marginT2.semTarefas.hide')
+    let isLoading = document.querySelector('.loaderDiv').style.display == 'inline'
+    if (isLoading){ //se estiver carregando proxima acao
+        console.log('carregandooooo')
+        window.setTimeout(comunicaComOInsta, 4000) //espera
+    }else if (comTarefas == null) { //ou seja, esta sem tarefas
+        let total = contagemPerfil.numeroConfirmados + contagemPerfil.numeroPulados
+        let stringAcoes = ''
+        stringAcoes += '========  TA SEM TAREFAS OPORA ========'
+        stringAcoes += '\n\nTOTAL CONFIRMADO ATE AGORA: ' + contagemPerfil.total
+        stringAcoes += '\n\nPerfil: ' + contagemPerfil.perfil
+        stringAcoes += '\nAcoes confirmadas: ' + contagemPerfil.numeroConfirmados
+        stringAcoes += '\nAcoes puladas: ' + contagemPerfil.numeroPulados
+        stringAcoes += '\nTotal: ' + total
+        console.log(stringAcoes)
+        contagemPerfil.perfil = ''
+        contagemPerfil.numeroConfirmados = 0
+        contagemPerfil.numeroPulados = 0
+        if (relogaPerfis){
+            logOut()
+        }
+    }else{
+        let objeto = {
+            'perfil': contagemPerfil.perfil,
+            'action': document.querySelector('.btn_light.cpointer.actionConectar p').textContent,
+            'link': document.querySelector('#conectar_step_4').href
+        }
+        let badURL = regExSiteInsta.test(objeto.link) == false
+        if(badURL){
+            console.warn('-> BAD URL: ' + objeto.link + '\nPulando tarefa...')
+            window.setTimeout(pulaTarefa, 2000)
+        }else{
+            wd.postMessage(objeto, 'https://www.instagram.com')
+            jQuery("#conectar_step_5").removeClass('d-none')
+        }
+    }
+}
+
+function confirmaTarefa(){
+    let botaoConfirmaTarefa = document.querySelector('#conectar_step_5 button')
+    botaoConfirmaTarefa.click()
+    contagemPerfil.numeroConfirmados++
+    contagemPerfil.total++
+    window.setTimeout(comunicaComOInsta, 13000)
+}
+
+function pulaTarefa(){
+    let botaoPulaTarefa = document.querySelector('.btn-dizu.bg-orange.sombra.marginT1.Bradius5.pularTarefa button')
+    botaoPulaTarefa.click()
+    contagemPerfil.numeroPulados++
+    window.setTimeout(comunicaComOInsta, 13000)
+}
+
+function logOut(){
+    let perfilAtual = getSelectedInstagram().text
+    listaDePerfis.forEach((element)=> {if(element.perfil == perfilAtual) element.done = true})
+    let proxPerfil = getAvailableInstagram()
+    if(proxPerfil == null){
+        console.warn('===== ACABOU OS PERFIS =====')
+        deixaPerfisDisponiveis()
+    }else{
+        printa('\n')
+        console.log('=== Logando no perfil ' + proxPerfil.perfil + ' ===')
+        let objeto = {
+            'perfil': proxPerfil.perfil,
+            'action': 'LogOut',
+            'link': 'https://www.instagram.com'
+        }
+        wd.postMessage(objeto, 'https://www.instagram.com')
+    }
+}
+
+const deixaPerfisDisponiveis = () => listaDePerfis.forEach((e, idx)=> idx == 0 ? e.done = true : e.done = false)
---
+++ insta.js
@@ -0,0 +1,150 @@
+window.addEventListener('message', receiveMessage, false);
+window.addEventListener('beforeunload', (event) =>{event.preventDefault(); event.returnValue= ''})
+let nomeDaJanelaFilha = 'janelaoTop'
+let janelaFilha = window.open('https://www.instagram.com', nomeDaJanelaFilha) // cria janela filha para nao ficar dando popup
+let siteDoDizu = 'https://dizu.com.br/painel/conectar'
+let regexPerfil = new RegExp()
+let proxPerfil = ''
+
+function receiveMessage(event){ //event listener
+    let objeto = event.data //pega o objeto que vem pelo evento
+    regexPerfil = new RegExp(objeto.perfil, 'i')
+    if (objeto.action == 'Seguir'){ //se for seguir
+        janelaFilha = window.open(objeto.link, nomeDaJanelaFilha) //atualiza a janela filha
+        try{
+            window.setTimeout(seguir, 10000) //manda seguir esperando 10 seg
+        }catch (e){
+            console.error(e.message)
+            console.warn('Tetando seguir dinovo...')
+            window.setTimeout(seguir, 10000)
+        }
+    }else if (objeto.action == 'Curtir'){ //se for curtir
+        janelaFilha = window.open(objeto.link, nomeDaJanelaFilha)
+        try{
+            window.setTimeout(curtir, 10000) //manda curtir esperando 10 seg
+        }catch (e){
+            console.error(e.message)
+            console.warn('Tetando curtir dinovo...')
+            window.setTimeout(curtir, 10000)
+        }
+    }else if (objeto.action == 'LogOut'){
+        proxPerfil = objeto.perfil
+        window.setTimeout(logOut, 1000)
+    }else if (objeto.action == 'clicarEntrar'){
+        proxPerfil = objeto.perfil
+        window.setTimeout(clicaEntrar, 1000)
+    }else{
+        console.log('nunca entre aqui')
+    }
+}
+
+function seguir(){
+    let pageNotAvailable = janelaFilha.document.getElementsByClassName('MCXLF')[0]
+    let botaoSeguir = janelaFilha.document.getElementsByClassName('_5f5mN       jIbKX  _6VtSN     yZn4P   ')[0]
+    let botaoSeguirSozinho = janelaFilha.document.getElementsByClassName('sqdOP  L3NKy   y3zKF     ')[0]
+    let isPrivate = janelaFilha.document.getElementsByClassName('_4Kbb_ _54f4m')[0]
+    let isPorno = janelaFilha.document
+    .getElementsByClassName('error-container -cx-PRIVATE-ErrorPage__errorContainer -cx-PRIVATE-ErrorPage__errorContainer__')[0]
+    let botaoSendMessage = janelaFilha.document.getElementsByClassName('fAR91 sqdOP  L3NKy _4pI4F   _8A5w5    ')[0]
+    let pageNotLoaded = (botaoSeguir == null) && (botaoSeguirSozinho == null)
+    pageNotLoaded = pageNotLoaded && (botaoSendMessage == null) && (isPrivate == null) && (isPorno == null)
+    pageNotLoaded = pageNotLoaded && (pageNotAvailable == null)
+    
+    
+    
+    if (pageNotLoaded){
+        window.setTimeout(seguir, 2000) //espera mais 2 segundos
+        return
+    }else{
+        if (isPorno != null || isPrivate != null || botaoSendMessage != null || pageNotAvailable != null){
+            window.setTimeout(mandaPularTarefa, 4000) //manda pular tarefa esperando 4 seg
+        }else{ //entao da pra clicar
+            let listaFotosPerfil = janelaFilha.document.getElementsByClassName('_6q-tv')
+            let nomeDoPerfil = listaFotosPerfil[listaFotosPerfil.length - 1].alt.split(' ').pop()
+            if(regexPerfil.test(nomeDoPerfil)){
+                if (botaoSeguir != null){
+                    botaoSeguir.click()
+                }else{
+                    botaoSeguirSozinho.click()
+                }
+                window.setTimeout(mandaConfirmar, 4000)
+            }else{
+                window.opener.postMessage('erroPerfil', siteDoDizu)
+            }
+        }
+    }
+}
+
+function curtir(){
+    let pageNotAvailable = janelaFilha.document.getElementsByClassName('MCXLF')[0]
+    let botaoCurtir = janelaFilha.document.getElementsByClassName('wpO6b ')[1]
+    let isPrivate = janelaFilha.document.getElementsByClassName('_4Kbb_ _54f4m')[0]
+    let isPorno = janelaFilha.document.getElementsByClassName('error-container -cx-PRIVATE-ErrorPage__errorContainer -cx-PRIVATE-ErrorPage__errorContainer__')[0]
+    let pageNotLoaded = (botaoCurtir == null) && (isPorno == null) && (isPrivate == null) && (pageNotAvailable == null)
+    
+    if (pageNotLoaded){
+        window.setTimeout(curtir, 2000)
+        return
+    }else{
+        if (botaoCurtir != null){
+            let listaFotosPerfil = janelaFilha.document.getElementsByClassName('_6q-tv')
+            let nomeDoPerfil = listaFotosPerfil[listaFotosPerfil.length - 1].alt.split(' ').pop()
+            if (regexPerfil.test(nomeDoPerfil)){
+                botaoCurtir.click()
+                window.setTimeout(mandaConfirmar, 4000)
+            }else{
+                window.opener.postMessage('erroPerfil', siteDoDizu)
+            }
+        }else{
+            window.setTimeout(mandaPularTarefa, 4000)
+        }
+    }
+}
+
+function mandaConfirmar(){
+    window.opener.postMessage('confirma', siteDoDizu)
+}
+
+function mandaPularTarefa(){
+    window.opener.postMessage('pula', siteDoDizu)
+}
+
+function logOut(){
+    let avatar = janelaFilha.document.getElementsByClassName('_6q-tv')
+    let botaoAvatar = avatar[avatar.length - 1]
+    botaoAvatar.click()
+    window.setTimeout(clicaSair, 4000)
+}
+
+function clicaSair(){
+    let sair = janelaFilha.document.getElementsByClassName('_7UhW9   xLCgt      MMzan  KV-D4              fDxYl     ')
+    let botaoSair = sair[sair.length - 1]
+    botaoSair.click()
+    window.setTimeout(clicaEntrar, 10000)
+}
+
+function clicaEntrar(){
+    let listaPerfis = janelaFilha.document.getElementsByClassName('l9hKg')
+    if (listaPerfis == null){
+        window.setTimeout(clicaEntrar, 4000)
+        console.warn('PERFIS NAO ENCONTRADO\nESPERANDO PERFIS....')
+    }else{
+        let id = -1
+        let tam = listaPerfis.length
+        for(let index = 0; index < tam; index++){
+            if (listaPerfis[index].textContent == proxPerfil){
+                id = index
+                break
+            }
+        }
+        if (id == -1){
+            window.opener.postMessage('erroAcharPerfil', siteDoDizu)
+        }else{
+            let botao = janelaFilha.document.getElementsByClassName('sqdOP  L3NKy   y3zKF     ')[id]
+            botao.click()
+            window.opener.postMessage('logou', siteDoDizu)
+        }
+    }
+}
+
+
